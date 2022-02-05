87ed0a7af7ae591dff2de456a3c9953ddd77dc06
As3ad Altabali <as3ad.moh@gmail.com>
2022-02-05 10:13:14 +0300

    update categories & products migrations

--- database/migrations/2022_02_05_070606_create_products_table.php
+++ database/migrations/2022_02_05_070606_create_products_table.php
@@ -15,7 +15,13 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price');
+            $table->string('avatar', 250)->nullable();
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
